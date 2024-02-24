ce399664d59840fb93bbd0fc437bf611315ae0ba
ivanDourado <jaivan96@protonmail.com>
2024-02-24 17:23:46 -0300

    Feat: trabalhando com xcom

--- dags/defaultargs.py
+++ dags/defaultargs.py
@@ -14,10 +14,11 @@ default_args = {
 
 dag = DAG('defaultargs', description="Dag de exemplo",
           default_args = default_args,
-           schedule_interval=None, start_date=datetime(2024,2,1),
-           catchup=False)
+           schedule_interval='@hourly', start_date=datetime(2024,2,1),
+           catchup=False, default_view='graph', tags=['processo','tag','pipeline'])
 
-task1 = BashOperator(task_id='tsk1', bash_command='sleep 5',dag=dag)
-task2 = TriggerDagRunOperator(task_id='tsk2', trigger_dag_id='dagrundag2',dag=dag)
+task1 = BashOperator(task_id='tsk1', bash_command='sleep 5',dag=dag, retries=3)
+task2 = BashOperator(task_id='tsk2', bash_command='sleep 5',dag=dag)
+task3 = BashOperator(task_id='tsk3', bash_command='sleep 5',dag=dag)
 
-task1 >> task2
+task1 >> task2 >> task3
---
+++ dags/xcom.py
@@ -0,0 +1,31 @@
+from airflow import DAG 
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime
+
+
+dag = DAG('exemplo_xcom', description="exemplo_xcom",
+           schedule_interval=None, start_date=datetime(2024,2,1),
+           catchup=False)
+
+def task_write(**kwargs):
+    kwargs['ti'].xcom_push(key='valorxcom1', value=10200)
+
+task1 = PythonOperator(
+    task_id='tsk1', 
+    python_callable=task_write,
+    dag=dag
+)
+
+def task_read(**kwargs):
+    valor = kwargs['ti'].xcom_pull(key='valorxcom1')
+    print(f'Valor recuperado: {valor} .')
+
+task2 = PythonOperator(
+    task_id='tsk2', 
+    python_callable=task_read,
+    dag=dag
+)    
+
+task1 >> task2
