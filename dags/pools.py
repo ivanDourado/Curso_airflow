2bdca3de4753741269aefd8b4048a32e346aac65
ivanDourado <jaivan96@protonmail.com>
2024-03-07 22:17:25 -0300

    pools

--- dags/pools.py
+++ dags/pools.py
@@ -1,21 +1,28 @@
 from airflow import DAG 
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import Variable
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
+""" Pools são usados para gerenciar a concorrência e a alocação de recursos. 
+ex: 
+- várias tarefas que precisam acessar um banco de dados. 
+- Limites de conexões e recursos;
+-você cria um pool que vai limitar e gerenciar o uso destas conexões.  
 
-dag = DAG('variaveis', description="variaveis",
-           schedule_interval=None, start_date=datetime(2024,2,1),
+o que será feito: 
+- criar 4 tasks 
+- criar 1 pool com 1 slot (worker disponivel para o recurso)
+(p pool vai gerenciar o uso do worker)
+
+- definir 'priority_weight para as tasks"""
+
+dag = DAG('pool', description="pool",
+           schedule_interval=None, start_date=datetime(2024,3,5),
            catchup=False)
 
-def print_variable(**context):
-    minha_var = Variable.get('minhavar')
-    print(f'O valor da variável é : {minha_var}')
+task1 = BashOperator(task_id='tsk1', bash_command='sleep 2',dag=dag, pool="meupool")
+task2 = BashOperator(task_id='tsk2', bash_command='sleep 3',dag=dag, pool="meupool", priority_weight=5)
+task3 = BashOperator(task_id='tsk3', bash_command='sleep 1',dag=dag, pool="meupool")
+task4 = BashOperator(task_id='tsk4', bash_command='sleep 1',dag=dag, pool="meupool", priority_weight=10)
 
-task1 = PythonOperator(
-    task_id='tsk1', 
-    python_callable=print_variable,
-    dag=dag
-)
 
-task1 
+
